8b0e92f50d6b659ca70b358bd024439e57b73316
Debajyoti Das <debo2696@gmail.com>
2018-04-25 09:56:38 +0530

    Add files via upload

---
+++ arr2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int arr[100],i;
+	printf("Enter a[0]-a[5]:\n");
+	scanf("%d",arr[0]);
+	scanf("%d",arr[1]);
+	scanf("%d",arr[2]);
+	scanf("%d",arr[3]);
+	scanf("%d",arr[4]);
+	scanf("%d",arr[5]);
+	for (i=6;i<=10;i++)
+	{
+		arr[i]=arr[i-5]*arr[i-4];
+		printf("arr[%d]:%d",i,arr[i]);
+		printf("\n");
+	}
+	return 0;
+	getch();
+}

0bde82f1631c6b986c88c92dcc33df4379e3eb0c
Debajyoti Das <debo2696@gmail.com>
2018-04-25 10:37:01 +0530

    Add files via upload

--- arr2.c
+++ arr2.c
@@ -1,21 +1,20 @@
-//This program's exiting on it's own after taking the 2nd input
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-	int arr[100],i;
-	printf("Enter a[0]-a[5]:\n");
-	scanf("%d",arr[0]);
-	scanf("%d",arr[1]);
-	scanf("%d",arr[2]);
-	scanf("%d",arr[3]);
-	scanf("%d",arr[4]);
-	scanf("%d",arr[5]);
+	int arr[100],i,b[3]={2,5,8};
+	printf("Enter a[0]-a[3]:\n");
+	scanf("%d",&arr[0]);
+	scanf("%d",&arr[1]);
+	scanf("%d",&arr[2]);
+	scanf("%d",&arr[3]);
+	scanf("%d",&arr[4]);
+	scanf("%d",&arr[5]);
+	printf("\n");
 	for (i=6;i<=10;i++)
 	{
 		arr[i]=arr[i-5]*arr[i-4];
-		printf("arr[%d]:%d",i,arr[i]);
-		printf("\n");
+		printf(" arr[%d]:%5d",i,arr[i]);
 	}
 	return 0;
 	getch();

17aa967457860fc75f44def37d9aca38ab98eda7
Debajyoti Das <debo2696@gmail.com>
2019-03-28 23:56:08 +0530

    Add files via upload

---
+++ mllc_pntr.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main()
+{
+	int *p,n,i;
+	printf("\nEnter no. of blocks:");
+	scanf("%d",&n);
+	p=(int *)malloc(4 * sizeof(int));
+	for(i=0;i<n;i++)
+	{
+		*(p+i)=i+10;
+	}
+	printf("\nShowing all values");
+	for(i=0;i<n;i++)
+	{
+		printf(" %d",*(p+i));
+	}
+	printf("\nShowing all addresses");
+	for(i=0;i<n;i++)
+	{
+		printf(" %d",(p+i));
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

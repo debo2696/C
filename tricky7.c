cc8b6838531f1f0c9c9ad18200d14be903761181
Debajyoti Das <debo2696@gmail.com>
2019-04-23 19:07:10 +0530

    Create tricky7.c

---
+++ tricky7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+/*{
+	int i=3,j;
+	j=++i*++i*++i;
+	printf("i:%d j:%d",i,j);
+}*/
+
+/*{
+	int i;
+	i=printf("Abcd");
+	printf("%d",i);
+}*/
+
+/*{
+	int i;
+	i=(2>9)?(4>5)?2:3+4:8;
+	printf("%d",i);
+}*/
+
+/*{
+	int i;
+	i=32>24<67>109;
+	printf("%d",i);
+}*/
+
+/*{
+	int x=3;
+	printf("%d",(x++)++);
+	printf("%d",(++x)++);
+	printf("%d",(x+1)++);
+}*/
+	
+
